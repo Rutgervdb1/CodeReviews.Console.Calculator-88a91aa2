e7e2e2b650c1e42895b2a684eed48902d07b5fe1
Rutgervdb1 <rutger.vdb@hotmail.com>
2026-04-15 15:05:07 +0200

    using more classes, fixing code spacings

--- Rutgervdb1.Callculator/Rutgervdb1.Callculator/Calculator.cs
+++ Rutgervdb1.Callculator/Rutgervdb1.Callculator/Calculator.cs
@@ -11,89 +11,31 @@ int calcUsesAmount = 1;
 bool useResults = false;
 double cleanNr1 = 0;
 double cleanNr2 = 0;
+var jsonFile = "jsoncalc.json";
 
-string? numInput1 = "";
-string? numInput2 = "";
-
-getCalculations getCalc = new getCalculations();
-
+Calculation getCalc = new Calculation();
+Calculator calc = new Calculator();
+var calculations = new List<Calculation>();
 
 Console.WriteLine($"Welcome to the C# calculator app!  \n");
 Console.WriteLine("-------------------------------- \n \n");
-
-Calculator calc = new Calculator();
-
-// create the json array to store operations
-
-JsonArray jsonArrayCalc = new JsonArray();
-
-
-
-// we want the program to continue as long as the user does not want to quit.
 
 while (!stopCalc) {
 
 
     double result = 0;
-    
-    bool validInt = false;
-    
-    // get a valid input for the first number.
-
+   
+   
     if(!useResults)
     {
-        do
-        {
-            if (!double.TryParse(numInput1, out cleanNr1) == true)
-            {
-                Console.WriteLine("Please give a valid integer number.");
-                numInput1 = Console.ReadLine();
-            }
-            else
-            {
-                validInt = true;
-
-            }
-
-        } while (validInt == false);
-
+        Console.WriteLine("Please give your fist integer number");
+        cleanNr1 = calc.ReadInput("Please give a valid integer number.");
     }
-
-   //----------------------------------------------------------------------------------------
 
     Console.WriteLine("Please write your second number: ");
 
-    numInput2 = Console.ReadLine();
-       
-    bool validInt2 = false;
-
-
-    // get a valid second number
-    // when using a while(!validint2), it loops one time to much
-    do
-    {
-
-        if (!double.TryParse(numInput2, out cleanNr2) == true)
-        {
-            Console.WriteLine("Please give a valid integer number.");
-            numInput2 = Console.ReadLine();
-
-        }
-        else
-        {
-            validInt2 = true;
-           
-          
-        }
-
-    } while (validInt2 == false);
-
-
-    //----------------------------------------------------------------------------------------
-
-
-    //display the options
-
+    cleanNr2 = calc.ReadInput("Please give a valid integer number.");
+   
     Console.WriteLine("Choose an operator from the following list:");
     Console.WriteLine("\ta - Add");
     Console.WriteLine("\ts - Subtract");
@@ -101,22 +43,17 @@ while (!stopCalc) {
     Console.WriteLine("\td - Divide");
     Console.Write("Your option? ");
 
-    // get the user choice
-     String? userChoice = Console.ReadLine();
+   
+     String? selectedOperator = Console.ReadLine();
 
 
-    // if the user didn't enter anything or it does not match and a/s/m/d
-    if (userChoice == null || !Regex.IsMatch(userChoice, "[asmd]"))
-    {
-
-        Console.WriteLine("This is not a valid input.");
-
+    if (selectedOperator == null || !Regex.IsMatch(selectedOperator, "[asmd]")){
+         Console.WriteLine("This is not a valid input.");
     }
-    else
-    {
+    else{
         try
         {
-            result = calc.doOperation(cleanNr1, cleanNr2, userChoice);
+            result = calc.doOperation(cleanNr1, cleanNr2, selectedOperator);
 
             if (double.IsNaN(result))
             {
@@ -130,37 +67,21 @@ while (!stopCalc) {
             Console.WriteLine("An exception ocurred while calculating: \n " + e.Message);
         }
 
-     //----------------------------------------------------------------------------------------
-
-        // Add data to the json Array
-
-
-        jsonArrayCalc.Add(new JsonObject
+        calculations.Add(new Calculation
         {
-            ["Number1"] = cleanNr1.ToString(),
-            ["Number2"] = cleanNr2.ToString(),
-            ["userChoice"] = userChoice,
-            ["Result"] = result.ToString()
+            num1 = cleanNr1,
+            num2 = cleanNr2,
+            SelectedOperator = selectedOperator,
+            result = result
         });
 
+        calc.SerializeJson(jsonFile, calculations);
 
-        // Write the data to the json file
-
-        File.WriteAllText("jsoncalc.json", jsonArrayCalc.ToJsonString(new JsonSerializerOptions
-        {
-            WriteIndented = true
-        }));
-
-
-     //----------------------------------------------------------------------------------------
 
         Console.WriteLine("------------------------\n");
         Console.WriteLine($"You've used the calculator {calcUsesAmount} times.");
         Console.WriteLine("Press enter to make another calculation,write " + "h" + " for you calculation history " + " or write " + "n" + " to quit.");
 
-
-
-        // Check if the user wants to quit or wants to see the calculation history
 
         switch (Console.ReadLine())
         {
@@ -170,80 +91,63 @@ while (!stopCalc) {
                 break;
 
             case "h":
-
-                String jsonHistory = File.ReadAllText("jsoncalc.json");
-                JsonArray jsonShowArray = JsonNode.Parse(jsonHistory)!.AsArray();
-
-                foreach (JsonNode jsonShow in jsonShowArray)
+                List<Calculation> history = calc.DeserializeJson(jsonFile);
+                
+                foreach (Calculation calculation in history)
                 {
 
-                    Console.WriteLine("------ Calculatie ------");
-                    Console.WriteLine($"nummer1: {jsonShow["Number1"]}");
-                    Console.WriteLine($"nummer2: {jsonShow["Number2"]}");
-                    Console.WriteLine($"Operation: {jsonShow["userChoice"]}");
-                    Console.WriteLine($"Result: {jsonShow["Result"]}");
+                    Console.WriteLine("------ Calculation ------");
+                    Console.WriteLine($"number1: {calculation.num1}");
+                    Console.WriteLine($"number2: {calculation.num2}");
+                    Console.WriteLine($"Operation: {calculation.SelectedOperator}");
+                    Console.WriteLine($"Result: {calculation.result}");
                     Console.WriteLine();
 
                 }
                 break;
-
         }
 
-        //check if the user stopped the calculator so it skips this step
-
-        if(stopCalc == false)
-        {
-
+        if(!stopCalc){
             Console.WriteLine("\n");
             Console.WriteLine("Would you like to use one of the the previous results for a new calculation ? Y/N");
 
-
             bool validInput = false;
 
-            do
-            {
-                string? usePrev = Console.ReadLine().ToLower();
+            do{
+                string? usePrev = Console.ReadLine()?.ToLower();
                
 
                 switch (usePrev)
                 {
 
                     case "y":
-
-                             // show history
-                        string jsonHistory = File.ReadAllText("jsoncalc.json");
-                        JsonArray jsonShowArray = JsonNode.Parse(jsonHistory)!.AsArray();
+                        List<Calculation> history = calc.DeserializeJson(jsonFile);
 
                         int index = 1;
-                        foreach (JsonNode item in jsonShowArray)
+                        foreach (Calculation calculation in history)
                          {
-                            Console.WriteLine($"[{index}] {item["Result"]}");
+                            Console.WriteLine($"[{index}] {calculation.result}");
                             index++;
                          }
 
                         Console.WriteLine("Choose a result number:");
-                        int chosenIndex = int.Parse(Console.ReadLine()!);
+                        int chosenIndex = int.Parse(Console.ReadLine());
                         bool rightIndex = false;
 
                         do
                         {
-                            if (chosenIndex > jsonShowArray.Count || chosenIndex < jsonShowArray.Count)
+                            if(chosenIndex > history.Count || chosenIndex < 1 )
                             {
-
-                                Console.WriteLine($"There is no result for the chosen array. please choose a number between 1 and {jsonShowArray.Count}");
-                                chosenIndex = int.Parse(Console.ReadLine());
-
+                                Console.WriteLine($"There is no data at the chosen number. Please choose a number between 1 and {history.Count()}.");
+                                chosenIndex = int.Parse(Console.ReadLine()) ;
                             }
-                            else
-                            {
-                                rightIndex = true;
-                            }
+                            else { rightIndex = true; }
 
 
                         } while (!rightIndex);
 
 
-                            cleanNr1 = double.Parse(jsonShowArray[chosenIndex - 1]!["Result"]!.ToString());
+                            cleanNr1 = int.Parse
 
                             Console.WriteLine($"Using {cleanNr1} as first number.");
 
